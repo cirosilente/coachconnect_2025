fa8fbd103f6d9c40d270c8492a2835bed1eed607
cirosilente <ciro.silente@gmail.com>
2025-06-19 12:32:06 +0200

    modifica al database.php

--- config/database.php
+++ config/database.php
@@ -75,8 +75,9 @@ return [
     'strict' => true,
     'engine' => null,
     'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
-    ]) : [],
+    PDO::MYSQL_ATTR_SSL_CA => base_path('storage/certs/ca.pem'),
+]) : [],
+
 ],
 
 
