b3eea5c1d78b57316b6158e6c9e3bd5600a38bd6
cirosilente <ciro.silente@gmail.com>
2025-06-20 01:36:58 +0200

    Update dependencies and configure Tailwind CSS with PostCSS

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +8,5 @@ export default defineConfig({
             refresh: true,
             buildDirectory: 'public/build',
         }),
-        tailwindcss(),
     ],
 });
