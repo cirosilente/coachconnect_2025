375f771706ee2a4715a2388598ad49b05b10b714
cirosilente <ciro.silente@gmail.com>
2025-06-19 16:35:48 +0200

    modifica dockerfile

--- config/database.php
+++ config/database.php
@@ -75,9 +75,8 @@ return [
     'strict' => true,
     'engine' => null,
     'options' => extension_loaded('pdo_mysql') ? array_filter([
-    PDO::MYSQL_ATTR_SSL_CA => base_path('storage/certs/ca.pem'),
-]) : [],
-
+        PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
+    ]) : [],
 ],
 
 
