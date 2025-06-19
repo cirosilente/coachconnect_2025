4adf4c46b891e7ba8b34f57ade6061ab956715fe
cirosilente <ciro.silente@gmail.com>
2025-06-19 23:41:09 +0200

    Fix namespace casing for Role enum in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Enums\role;
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
