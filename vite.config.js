32cff247b0e6d7f913b4403ca2f0acf62b74ffaf
cirosilente <ciro.silente@gmail.com>
2025-06-20 02:57:30 +0200

    Update Vite configuration to specify build output directory and enable empty output directory

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            buildDirectory: 'public/build',
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
 });
